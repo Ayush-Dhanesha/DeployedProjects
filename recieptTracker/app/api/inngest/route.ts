792fffc36cd1c2691090d8aba9347953957c1ca6
lonelyking007 <22ucs065@lnmiit.ac.in>
2025-07-03 00:03:57 +0530

    Vercel1

--- recieptTracker/app/api/inngest/route.ts
+++ recieptTracker/app/api/inngest/route.ts
@@ -6,6 +6,6 @@ import { extractAndSavePDF } from "@/inngest/agent";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    extractAndSavePDF,
+    extractAndSavePDF
   ],
 });
